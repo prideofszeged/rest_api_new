5f45d6a9b1c20a40ba8ba6689f36c471860b6d74
Steve D <stmada@gmail.com>
2017-01-22 12:09:39 -0600

    adding items to database

--- item.py
+++ item.py
@@ -3,9 +3,12 @@ from flask_jwt import jwt_required
 import sqlite3
 
 
+
 #Flask restful jsonifies stuff for us
 # student class inherits from Resource
 class Item(Resource):
+    TABLE_NAME = 'items'
+
     parser = reqparse.RequestParser()
     parser.add_argument('price',
                         type=float,
@@ -26,16 +29,44 @@ class Item(Resource):
 
         return {'message': 'Item not found'}, 404
 
+    @classmethod
+    def find_by_name(cls, name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "Select * from {table} where name =?".format(table=cls.TABLE_NAME)
+        result = cursor.execute(query, (name,))
+        row = result.fetchone()
+        connection.close()
+
+        if row:
+            return {'item': {'name': row[0], 'price': row[1]}}
 
     def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None):
+        if self.find_by_name(name):
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
+
         data = Item.parser.parse_args()
 
-        #data = request.get_json(silent=True)
         item = {'name': name, 'price': data['price']}
-        items.append(item)
-        return item, 201
+
+        try:
+            Item.insert(item)
+        except:
+            return {"message": "An error occurred inserting the item"}
+
+        return item
+
+    @classmethod
+    def insert(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO {table} VALUES (NULL, ?, ?)".format(table=cls.TABLE_NAME)
+        cursor.execute(query, (item['name'], item['price']))
+
+        connection.commit()
+        connection.close()
 
     def delete(self, name):
         global items
